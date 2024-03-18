2452a16843fc6bb34876eb617c16e7a6493dd402
emanuele-pogliari <manu.pogliari@gmail.com>
2024-03-18 19:55:09 +0100

    add background and min and max input range

--- index.php
+++ index.php
@@ -60,6 +60,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>Hotel PHP</title>
 </head>
 <body>
@@ -72,7 +73,7 @@
 
 <div class="mb-3 col-4">
   <label for="vote" class="form-label">Insert vote rate:</label>
-  <input type="number" class="form-control" id="vote" placeholder="Vote value" name="vote">
+  <input type="number" class="form-control" id="vote" placeholder="Vote value" name="vote" min="1" max="5">
 </div>
 
   <input class="form-check-input" type="checkbox" value="false" id="parking-flag" name="parking-flag">
